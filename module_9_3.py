e2c7311f5fd65a3e9200e8132bd59239e5ddc915
Evgeny Zvidennyy <toteche@gmail.com>
2024-10-24 11:29:36 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,12 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(f) - len(s) for f, s in zip(first, second) if len(f) != len(s))
+
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+# Проверка first_result
+print(list(first_result))
+
+# Проверка second_result
+print(list(second_result))
