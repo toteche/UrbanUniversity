213bb773a5b74bc493c11cd5bf7e9f8e4078ef47
Evgeny Zvidennyy <toteche@gmail.com>
2024-10-28 22:59:51 +0300

    Add files via upload

--- module_9_4.py
+++ module_9_4.py
@@ -1,17 +1,17 @@
-# first = 'Мама мыла раму'
-# second = 'Рамена мало было'
-# result = list(map(lambda x, y: x == y, first, second))
-# print(result)
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
 
-# def get_advanced_writer(file_name):
-#     def write_everything(*data_set):
-#         with open(file_name, 'a') as file:
-#             for data in data_set:
-#                 file.write(str(data))
-#     return write_everything
-#
-# write = get_advanced_writer('example.txt')
-# write('Это строчка', ['А', 'это', 'уже', 'число', '5', 'в', 'списке'])
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a') as file:
+            for data in data_set:
+                file.write(str(data))
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', '5', 'в', 'списке'])
 
 from random import choice
 
